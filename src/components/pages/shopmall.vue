351b0f460ac8696f81ebc86ea3b1fc06bbf22b72
mbackham <634264926@qq.com>
2018-12-24 22:31:35 +0800

    home2

--- src/components/pages/shopmall.vue
+++ src/components/pages/shopmall.vue
@@ -41,33 +41,24 @@
 						<div class="recommend-item">
 							<img :src="item.image" width="80%" >
 							<div>{{item.goodsName}}</div>
-							<div>￥{{item.price}} (￥{{item.mallPrice}})</div>
+							<div>￥{{item.price | moneyFilter}} (￥{{item.mallPrice | moneyFilter}})</div>
 						</div>
 					</swiper-slide>
 				</swiper>
 			</div>
 		</div>
-		<div class="floor">
-			<div class="floor-normal">
-				<div class="floor_one">
-					<img :src="floor1_0.image" width="100%"/>
-				</div>
-				<div>
-					<div class="floor_two">
-						<img :src="floor1_1.image" width="100%"/>
-					</div>
-					<div >
-						<img :src="floor1_2.image" width="100%">
-					</div>
-				</div>
-			</div>
-			
-		</div>
+		<floor :floorData='floor1' :floorTitle='floorName.floor1'></floor>
+		<floor :floorData='floor2' :floorTitle='floorName.floor2'></floor>
+
+		<floor :floorData='floor3' :floorTitle='floorName.floor3'></floor>
+
 	</div>
 </template>
 <script>
 	import axios from 'axios'
 	import 'swiper/dist/css/swiper.css'
+	import floor from '../floor/floor'
+	import {toMoney} from '@/filter/moneyFilter.js'
 	import {swiper,swiperSlide} from 'vue-awesome-swiper'
 	export default{
 		data(){
@@ -83,14 +74,20 @@
                 	slidesPerView:3
                 },
                 floor1:[],
-                floor1_0:{},
-                floor1_1:{},
-                floor1_2:{}
+                floor2:[],
+                floor3:[],
+              
+                floorName:{}
 
 			}
 		},
+		filters:{
+			moneyFilter(money){
+				return toMoney(money)
+			}
+		},
 		components:{
-			swiper,swiperSlide
+			swiper,swiperSlide,floor
 		},
 		created(){
 			axios({
@@ -104,18 +101,16 @@
 				this.banner = res.data.data.slides
 				this.recommongoods=res.data.data.recommend
 				this.floor1=res.data.data.floor1
-				this.floor1_0 = this.floor1[0]
-                this.floor1_1 = this.floor1[1]
-                this.floor1_2 = this.floor1[2]
+				this.floor2=res.data.data.floor2
+				this.floor3=res.data.data.floor3
+
+				this.floorName=res.data.data.floorName
 				console.log(res.data.data)
 			}).catch(err=>{
 				console.log(err)
 			})
 		},
-		components:{
-				
-			swiper,swiperSlide
-		}
+	
 	}
 </script>
 <style>
@@ -183,22 +178,5 @@
 		text-align:center;
 
 	}
-	.floor-normal{
-		 display:flex;
-		 flex-direction:row;
-		 background:#fff;
-		 border-bottom:1px solid #ddd;
-
-	}
-	.floor-normal div {
-		width:10rem;
-		box-sizing:border-box;
-	}
-	.floor_one{
-		border-right:1px solid #ddd;
-
-	}
-	.floor_two{
-		border-bottom:1px solid #ddd;
-	}
+	
 </style>
